aa9795dd3e9874743be0f47ad9df1396d2c22842
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-03 17:44:05 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using callingControllerUsingJavaScript.Models;
+
+namespace callingControllerUsingJavaScript.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(PersonModel person)
+        {
+            int personId = person.PersonId;
+            string name = person.Name;
+            string gender = person.Gender;
+            string city = person.City;
+            return Json(person);
+        }
+    }
+}
